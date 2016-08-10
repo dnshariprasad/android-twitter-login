05f5fe32a72441d2f3a04805728102acaf69d987
Hari Prasad <hari.kvana@gmail.com>
2016-08-10 18:47:20 +0530

    first commit

---
+++ app/src/main/java/com/twitterlogin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.twitterlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+    git remote add origin https://github.com/dnshariprasad/android-twitter-login.git
